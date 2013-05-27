9c7654f40131d1a50aa5ad80159783597b221384
Dima Ostapenko <1rpamm@gmail.com>
2013-05-27 23:17:00 +0400

    fix updater

--- update.sh
+++ update.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 TIMESTAMP=$(date +"%Y\%m\%d %H:%M:%S")
-COMMIT="new repo; clean up" #~($TIMESTAMP)"
+COMMIT="fix updater" #~($TIMESTAMP)"
+
+#~ git add .
+#~ git rm -rf tmp/
+#~ git commit -m "$COMMIT"
+#~ git remote add origin git@github.com:1rpamm/equipment-system.git
+#~ git push -u origin master
 
 git add .
 git rm -rf tmp/
 git commit -m "$COMMIT"
-git remote add origin git@github.com:1rpamm/equipment-system.git
 git push -u origin master
-
-#~ git add .
-#~ git commit -m "$COMMIT"
-#~ git push -u origin master
 #~ git push --force --progress origin master
