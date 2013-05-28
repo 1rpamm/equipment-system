5e36b5006a2c4261494e02d14ec269e1defc66c4
Dima Ostapenko <1rpamm@gmail.com>
2013-05-29 03:52:14 +0400

    updater fix

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 TIMESTAMP=$(date +"%Y\%m\%d %H:%M:%S")
 COMMIT="search installed" #~($TIMESTAMP)"
 
+echo -n "Commit > "
+read commit
+
 #~ git add .
 #~ git rm -rf tmp/
 #~ git commit -m "$COMMIT"
@@ -12,6 +15,6 @@ COMMIT="search installed" #~($TIMESTAMP)"
 git add .
 git rm -rf tmp/
 git rm -rf log/
-git commit -m "$COMMIT"
-git push -u origin master
-#~ git push --force --progress origin master
+git commit -m "$commit"
+#git push -u origin master
+git push --force --progress origin master
